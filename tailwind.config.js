b675a06113f8da464a32ddc05e6495e5587d3959
Neaz-mq <neazmorshed666@gmail.com>
2026-05-04 13:01:55 +0600

    updated tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
       lg: '1024px',
       xl: '1280px',
       '2xl': '1536px',
-      '1920': '1920px',   // ← add this
+      '1920': '1920px',   
       '3xl': '2560px',
     },
     extend: {
